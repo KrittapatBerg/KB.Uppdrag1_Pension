a5b0b42515f7b89991c79fbe9ca8a523d2c5e5e5
Krittapat <Krittapat@DESKTOP-D695PKV>
2022-02-13 11:32:24 +0100

    Add project files.

---
+++ KB.Uppdrag1_Pension/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KB.Uppdrag1_Pension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vad heter du? Namn: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Efternamn: ");
+            string lastname = Console.ReadLine();
+
+            Console.Write("Hur gammal är du? Svar: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int retireage = 65;
+            int pension = retireage - age;
+            
+            Console.Clear();
+            Console.WriteLine("Hej " + name + " " + lastname + "!" + " Du är " + age + " år gammal.");
+            
+            if (age >= retireage)
+            {
+                Console.WriteLine("Du är pensionär.");
+            } 
+            else 
+            {
+                Console.WriteLine("Nu har du " + pension + " år kvar till du går i pension.");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
